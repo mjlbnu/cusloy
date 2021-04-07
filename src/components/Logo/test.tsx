a210bda193b0ab34d6232e59419ab8cd735612a8
Marcio Lisboa <mjlbnu@gmail.com>
2021-04-06 22:49:35 -0300

    Logo OK

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Logo from '.'
 
@@ -22,5 +23,39 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Loyalty/i).parentElement).toHaveStyle({
       color: '#030517'
     })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel...
+    // expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Loyalty/i).parentElement).toHaveStyle({
+      width: '11.3rem'
+    })
+  })
+
+  it('should render a big logo', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel...
+    // expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Loyalty/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a big logo without text if hideOnMobile', () => {
+    // renderizar o componente 'render'
+    // selecionar o elemento a ser testado 'screen' (queries) - getByLabel...
+    // expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Loyalty/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.9rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
